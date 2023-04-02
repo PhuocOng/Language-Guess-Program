874a84a8d57ff99ff15be5a4058b3c7cd42c3aa7
Peter Ong <onggiaphuoc2004@gmail.com>
2023-04-02 11:58:46 -0400

    create Language Determine Program

---
+++ index.js
@@ -0,0 +1,17 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+ 
+import { franc } from "franc";
+const langs = require('langs');
+const colors = require('colors');
+const input = process.argv[2];
+const langCode = franc(input);
+
+console.log(langCode)
+if (langCode === "und") {
+    console.log("Sorry, Couldn't Figure It Out!");
+} else {
+    const language = langs.where("3", langCode);
+    console.log( `Our best guess is ${language.name}`);
+}
+
